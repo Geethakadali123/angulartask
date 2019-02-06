4067cada8014dac7ec065356c6fbf66a1dfa9c06
gekadal1 <gekadal1@in.ibm.com>
2019-02-06 18:01:36 +0530

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardcontainerComponent } from './track/cardcontainer/cardcontainer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CardcontainerComponent,
+    data: { country: 'India'}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
